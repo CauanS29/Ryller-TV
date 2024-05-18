499e259a8e9d2d78033675aa939a28fac70ecb59
joaopaulo0007 <joaomendonca865@gmail.com>
2024-05-18 13:47:47 -0300

    feat/(Login):adicionei os imputs na tela Login

--- ryller-tv/src/app/Login/page.tsx
+++ ryller-tv/src/app/Login/page.tsx
@@ -1,12 +1,18 @@
 import Form from '../shared/components/Form/Form';
 
+
+
 export default function telaLogin() {
+   
     return (
-        <div className="relative bg-black">
-            <div className="absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black/10 to-black" />
+
+
+        <div className="relative top-0 left-0 w-full h-screen  bg-blue-900">
             <div className="relative">
                 <Form />
             </div>
         </div>
+
+
     );
 }
--- ryller-tv/src/app/shared/components/Form/Form.tsx
+++ ryller-tv/src/app/shared/components/Form/Form.tsx
@@ -1,12 +1,16 @@
 'use client'
-
+import Input from "../Input/Input"
+import { useState } from "react"
+import Button from "../Button/Button"
 export default function Form(){
-
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+  
     return(
         <div className="flex items-center justify-center">
             <form>
-                <h1>Login</h1>
-                <p>Faça seu login aqui.</p>
+                <Input id="input-Email" label="EMAIL:*" placeHolder="digite seu Email" onChange={(e) => setEmail(e.target.value)} value={email} />
+                <Input id="input-Password" label="SENHA:*" placeHolder="digite sua Senha" onChange={(e) => setPassword(e.target.value)} value={password} />
             </form>
         </div>
     )
